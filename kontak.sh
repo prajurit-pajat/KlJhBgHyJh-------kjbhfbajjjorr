53e19f676af3ff67faf871017241c2eb4892eca7
prajurit-pajat <117523991+prajurit-pajat@users.noreply.github.com>
2022-11-05 21:00:12 +0700

    Add files via upload

---
+++ kontak.sh
@@ -0,0 +1,347 @@
+#!/bin/usr/bash
+# copyright by panglima jateng
+# open source
+
+# code warna
+birut='\e[1;34m'
+hijau='\e[1;32m'
+biru='\e[1;36m'
+merah='\e[1;31m'
+pink='\e[1;35m'
+kuning='\e[1;33m'
+putih='\e[1;37m'
+# penghapus teks
+clear
+# variabel
+xg=xdg-open
+cnl=https://youtube.com/channel/UCSJohuQCtqfD2P73Z65g8jg
+a=pa
+b=li
+c=ja
+d=ng
+e=ma
+f=te
+ind=+62
+at=author
+tm=team
+sc=script
+echo -e $kuning
+# mengetik
+ment="  selamat datang di script $a$d$b$e $c$f$d"
+for i in {1..100}; do
+      echo -e -ne "\r${ment:0:$i}";
+      sleep 0.05
+
+done
+echo -e $hijau
+sleep 1
+# teks asci
+figlet KONTAK CW
+# sleep or durasi
+sleep 1
+echo -e $merah"             +++++"$biru"$a$d$b$e $c$f$d"$merah"+++++"
+echo
+# garis
+drut="======================================================"
+sleep 1
+    # daftar isi
+    echo -e $biru"<"$pink" 1"$biru" >"$putih" •"$kuning" Subcribe Autor"
+    echo -e $biru"<"$pink" 2"$biru" >"$putih" •"$kuning" Kontak Cewek"
+    echo -e $biru"<"$pink" 3"$biru" >"$putih" •"$kuning" join to grup"
+    echo -e $biru"<"$pink" 4"$biru" >"$putih" •"$kuning" lapor bug"
+    echo -e $hijau$drut$biru
+    sleep 1
+       # pengisin
+       read -p "input > " put;
+# database 1
+if [[ $put == "1" ]]; then
+s=sleep
+echo -e $hijau
+sub="terimakasih jika anda mau subcribe cenel saya"
+for i in {1..100}; do
+      echo -e -ne "\r${sub:0:$i}";
+      sleep 0.05
+
+done
+echo
+$s 2
+$xg $cnl
+bash kontak.sh
+fi
+# database 2
+if [[ $put == "2" ]]; then
+nx="____________________________________________________________"
+s=sleep
+$s 1
+co=echo
+$co
+$co -e $biru"       {"$kuning"•"$biru"}"$pink" Author"$hijau" :"$pink"  Panglima Jateng"
+$co -e $biru"       {"$kuning"•"$biru"}"$pink" Team  "$hijau" :"$pink"  Mafia Teknologi"
+$co -e $biru"       {"$kuning"•"$biru"}"$pink" Script"$hijau" :"$pink"  Kontak Cewek"
+$co -e $biru"#############################################"
+$co -e $hijau
+sleep 1
+$co "1. Arty : +62 822-8462-3351"
+$co
+$s 1
+$co "2. Caca : +62 858-5059-9149"
+$s 1
+$co
+$co "3. Latip: +62 821-7169-4128"
+$s 1
+$co
+$co "4. KIA : +62 852-9871-1321"
+$s 1
+$co
+$co "5. Dinda : +62 881-0371-00206"
+$s 1
+$co
+$co "6. Sarah : +62 813-9175-6710"
+$s 1
+$co
+$co "7. Mayang : +62 831-9845-2419"
+$s 1
+$co
+$co "8. Fitri : +62 822-7415-8207"
+$s 1
+$co
+$co "9. Naisa : +62 815-2572-3170"
+$s 1
+$co
+$co "10. Amelia : +62 838-6348-6501"
+$s 1
+$co
+$co "11. Shima : +60 17-800 5929"
+$s 1
+$co
+$co "12. Mona : +62 813-1645-7443"
+$s 1
+$co
+$co "13. Salsa : +62 812-6145-2429"
+$s 1
+$co
+$co "14. Queen : +62 812-9634-7598"
+$s 1
+$co
+$co "15. Priska : +62 813-5062-4393"
+$s 1
+$co
+$co "16. Nadia : +62 878-3107-4340"
+$s 1
+$co
+$co "17. Devi : +62 895-4288-10300"
+$s 1
+$co
+$co "18. Nurul : +62 821-8152-5846"
+$s 1
+$co
+$co "19. Chika : +62 859-1804-11835"
+$s 1
+$co
+$co "20. Putri : +62 831-2870-0003"
+$s 1
+$co
+$co "21. Cici : +62 852-3617-0356"
+$s 1
+$co
+$co "22. Lala : +852 5740 7530"
+$s 1
+$co
+$co "23. Angel : +62 878-3712-4666"
+$s 1
+$co
+$co "24. Desti : +62 821-1842-6491"
+$s 1
+$co
+$co "25. Yuni : +62 882-2007-8226"
+$s 1
+$co
+$co "26. Elin : +62 857-7438-1883"
+$s 1
+$co
+$co "27. Firda : +62 857-4757-1147"
+$s 1
+$co
+$co "28.Saputri galih : +62 859-3856-8681"
+$s 1
+$co
+$co "29. Lulu : +62 812-7849-0875"
+$s 1
+$co
+$co "30. Rahma : +62 895-6165-94030"
+$s 1
+$co
+$co "31. Salsa : +62 838-9624-7081"
+$s 1
+$co
+$co "32. Shila : +62 882-0063-81233"
+$s 1
+$co
+$co "33. Tasya : +62 859-1804-11835"
+$s 1
+$co
+$co "34. Sintia : +62 857-7457-4924"
+$s 1
+$co
+$co "35. presy : +62 859-5523-9146"
+$s 1
+echo
+echo "36. risna : +62 831-7693-5578"
+sleep 1
+echo
+echo "37. mutia : +62 853-7419-1807"
+sleep 1
+echo
+echo "38. karima : +62 819-0831-4000"
+sleep 1
+echo
+echo "39. adnya : +62 895-4149-50109"
+sleep 1
+echo
+echo "40. melisa : +62 812-3032-0545"
+sleep 1
+echo
+echo "41. uzi najwa : +62 812-7684-4587"
+sleep 1
+echo
+echo "42. aini : +62 813-6468-3713"
+sleep 1
+echo
+echo "43. keiska : +62 823-7140-4738"
+sleep 1
+echo
+echo "44. nafisah : +62 831-6782-3368"
+sleep 1
+echo
+echo "45. syarifa : +62 838-0065-0719"
+sleep 1
+echo
+echo "46. sri : +62 852-7462-8528"
+sleep 1
+echo
+echo "47. anita : +62 853-3601-7949"
+sleep 1
+echo
+echo "48. rahayu : +62 856-5052-261"
+sleep 1
+echo
+echo "49. dahlia : +62 857-1465-6705"
+sleep 1
+echo
+echo "50. devani ; +62 857-4007-0336"
+sleep 1
+echo
+echo "51. nabila : +62 882-7944-3651"
+sleep 1
+echo
+echo "52. asiyfa : +62 895-2127-1724"
+sleep 1
+echo
+echo "53. mei anggreni : +62 856-5891-3306"
+sleep 1
+echo
+echo "54. fara : +62 857-6671-3389"
+sleep 1
+echo
+echo "55. Tasya : +62 822-7313-9167"
+sleep 1
+echo
+echo "56. Sita : +62 856-4310-8003"
+sleep 1
+echo
+echo "57. Kartika : +62 838-5353-1722"
+sleep 1
+echo
+echo "58. Sofia : +62 856-9307-1588"
+sleep 1
+echo
+echo "59. Hani : +62 857-3823-2419"
+sleep 1
+echo
+echo "60. Wina : +62 858-1724-0544"
+sleep 1
+echo
+echo "61. Cya : +62 858-3263-0114"
+sleep 1
+echo
+echo "62. Salwa : +62 815-1187-8687"
+sleep 1
+echo
+echo "63. Aulia : +62 812-6181-8668"
+sleep 1
+echo
+echo "64. Jesika : +62 831-0355-1915"
+sleep 1
+echo
+echo "65. Yura : +62 858-7184-0166"
+sleep 1
+echo
+echo "67. Nadia : +62 895-3701-77062"
+sleep 1
+echo
+echo "68. Nazla : +62 812-1253-0203"
+sleep 1
+echo
+echo "69. Widia : +62 812-3011-8096"
+sleep 1
+echo
+echo "70. Novi : +62 812-4585-9556"
+sleep 1
+echo
+echo "71. Aulia : +62 857-4964-3481"
+sleep 1
+echo
+echo "72. Mei : +62 856-5891-3306"
+sleep 1
+echo
+echo "73. Fika : +62 881-3258-688"
+sleep 1
+echo
+echo "74. Cinta : +62 831-0355-1915"
+sleep 1
+echo
+echo "75. Dinda : +62 857-2400-2463"
+sleep 1
+echo
+echo "76. Inayah : +62 838-4564-1972"
+sleep 1
+echo
+echo "77. Cw 1 : +62 812-6702-5892"
+sleep 1
+echo
+echo "78. Cw 2 : +62 813-6468-3713"
+sleep 1
+echo
+echo "79. Cw 3 : +62 823-7140-4738"
+sleep 1
+echo
+echo "80. Cw 4 : +62 853-6670-4402"
+sleep 1
+echo
+echo "81. Cw 5 : +62 856-0718-8932"
+sleep 1
+echo
+echo "82. Cw 6 : +62 856-4322-5354"
+sleep 1
+echo
+echo "83. Cw 7 : +62 856-4966-8798"
+sleep 1
+echo
+echo "84. Cw 8 : +62 812-7212-8485"
+sleep 1
+echo
+echo "85. Cw 9 : +62 813-4015-9096"
+fi
+# database 3 login to grup
+if [[ $put == '3' ]]; then
+xdg-open https://chat.whatsapp.com/KDi54hngTNl8hUxD5CiGUL
+sleep 1
+bash kontak.sh
+fi
+# database 4 lapor bug
+if [[ $put == '4' ]]; then
+xdg-open http://Wa.me/+62857-1681-7375
+sleep 1
+bash kontak.sh
+fi
+
