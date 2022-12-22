b837be9c1f2e36729aa93b459fd15c9706495390
prajurit-pajat <117523991+prajurit-pajat@users.noreply.github.com>
2022-12-22 21:36:40 +0700

    kontak.sh

---
+++ nomor-cewe.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+# code by trio renkarnasi
+$_mr_vurus_spm
+$_panglima_jateng
+$_prajurit_pajat
+# daftar isi / formulir
+echo
+echo "{•} Silahkan Isi Formulir"
+echo "------------------------------------------"
+read -p "> Nama    : " nama;
+read -p "> Team    : " team;
+sleep 2
+# tampilan
+clear
+figlet KONWHASCE
+echo "              {•} code by trio renkarnasi 2023 {•}"
+echo
+echo "{•} pengguna script!!!"
+echo "-----------------------------------"
+echo "{+} Nama : $nama"
+echo "{+} Team : $team"
+echo "-----------------------------------"
+echo "(mr.virus.spm) (panglima.jateng) (prajurit.pajat)"
+echo "——————————————————————————————————————————————————————"
+echo "           {×××××}DAFTAR•ISI•SCRIPT{×××××}"
+echo "÷=======÷÷÷==============÷÷÷==============÷÷÷========÷"
+echo "  [01]. kontak cewe"
+echo "  [02]. subcribe author"
+echo "  [03]. lapor bug"
+echo "  [04]. donasi kontak cewe"
+echo "÷=======÷÷÷==============÷÷÷==============÷÷÷========÷"
+echo
+read -p "choose >>> " chos;
+if [[ $chos == "01" ]] || [[ $chos == "1" ]]; then
+echo
+echo "identitas : amalia / +62 838-6348-6501"
+echo "identitas : mei / lampung / +62 856-5891-3306"
+echo "identitas : dila / jateng / 2008 / +62 857-4007-0336"
+echo "identitas : yura / bandung / 2007 / +62 858-7184-0166"
+echo "identitas : caca / 2010 / +62 858-5059-9149"
+echo "identitas : jesica / semarang / 2006 / +62 831-0355-1915"
+echo "identitas : hartati / jakarta / 2008 / +62 838-5353-1722"
+echo "identitas : melisa / jatim / +62 812-3032-0545"
+echo "identitas : nadia / 2005 / +62 895-3701-77062"
+echo "identitas : vika / 2006 / +62 881-3258-688"
+echo "identitas : iqlima / 2008 / +62 812-7212-8485"
+echo "identitas : dahlia / 2008 / jakarta / +62 857-1465-6705"
+echo "identitas : ara / 2006 / lampung / +62 857-6671-3389"
+echo "identitas : salwa / 2009 / depok / +62 815-1187-8687"
+echo "identitas : cya / 2010 / lampung / +62 858-3263-0114"
+echo "identitas : sohani / lombok / +62 857-3823-2419"
+echo "identitas : nia / +62 813-4015-9096"
+echo "identitas : aini / sumatra barat / +62 813-6468-3713"
+echo "identitas : nih cewe asli cantik bet / +62 812-9634-7598"
+echo "identitas : nurul / +62 821-8152-5846"
+echo "identitas : nabila / +62 882-7944-3651"
+echo "identitas : tika / +62 822-7313-9167"
+echo "identitas : rifa / +62 838-0065-0719"
+echo "identitas : nisa / +62 852-3617-0356"
+echo "identitas : dinda / +62 881-0371-00206"
+echo "identitas : karima / +62 819-0831-4000"
+echo "identitas : moona / +62 813-1645-7443"
+echo "identitas : tasya / 2008 / banyuwangi / +62 859-1804-11835"
+echo "identitas : rindu /2006 / pemalang / +62 831-9836-3357"
+echo "identitas : devi / +62 895-4288-10300"
+echo "identitas : chalista / +62 899-9318-040"
+echo "identitas : nazwa / +62 8952-6818-140"
+echo "identitas : vita / +62 8537-5892-634"
+echo "identitas : putri / +62 8238-7453-506"
+echo "identitas : kenti / +62 8231-2006-376"
+echo "identitas : shela / +62 8226-7781-985"
+echo "identitas : melani / +62 8226-8623-391"
+echo "identitas : anita / +62 8231-0145-622"
+echo "identitas : cecew / +62 8229-7514-098"
+echo "identitas : tika / +62 8526-4669-920"
+echo "identitas : putri / +62 8228-8212-140"
+echo "identitas : maya / +62 8537-9034-480"
+echo "identitas : sari / +62 8951-3300-898"
+echo "identitas : nur / +62 8226-0349-690"
+echo "identitas : ayu / +62 8526-4799-931"
+echo "identitas : bela / +62 8127-5638918"
+echo "identitas : safira / +62 8228-7107-880"
+echo "identitas : nisya / +62 8136-1491-334"
+echo "identitas : mifta /+62 8229-7875-701"
+echo "identitas : mimy / +62 8129-6008-742"
+echo "identitas : mira / +62 8135-6846-709"
+echo "identitas : cia / +62 881-7885-963"
+echo "identitas : putry / +62 812-7843-2393"
+echo "identitas : +62 813-3937-6763"
+echo "identitas : +62 813-5865-6226"
+echo "identitas : azizah / +62 821-1487-6492"
+echo "identitas : rahayu / +62 822-3848-2677"
+echo "identitas : +62 822-7474-7139"
+echo "identitas : +62 838-2465-4688"
+echo "identitas : +62 852-7802-7004"
+echo "identitas : +62 856-0895-5311"
+echo "identitas : +62 856-4105-9125"
+echo "identitas : +62 858-6623-7211"
+echo "identitas : +62 858-1135-8608"
+echo "identitas : rara / kalteng / +62 812-5685-0676"
+echo "identitas : dinda / 2012 / bekasi / +62 821-2357-1834"
+echo "identitas : mutia / medan / +62 823-6380-1881"
+echo "identitas : dila / +62 822-7828-4376"
+echo "identitas : cela / +62 859-2531-8781"
+echo "identitas : ratih / +62 858-7678-2758"
+echo "identitas : dinda / jateng"
+elif [[ $chos == "2" ]] || [[ $chos == "02" ]]; then
+xdg-open https://youtube.com/@panglimajateng8875
+bash nomor-cewe.sh
+elif [[ $chos == "3" ]] || [[ $chos == "4" ]] || [[ $chos == "04" ]] || [[ $chos == "03" ]]
+then
+xdg-open http://Wa.me/+62882-1217-1828
+else
+exit
+fi
